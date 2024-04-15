40b3732d02778550188873d4d87a90c7b27587b9
dora <bigpowerpig@gmail.com>
2024-04-15 22:43:57 +0800

    init

---
+++ src/components/HeaderNavbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="w-full">
+      <a href="#">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <span class="ml-3 text-2xl">Your Company</span>
+      </a>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  setup() {
+    return {
+
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
