508214bf640025e1923fe9daaa0f8f7382e7fc83
dora <bigpowerpig@gmail.com>
2024-04-16 17:49:27 +0800

    header

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -1,9 +1,32 @@
 <template>
-  <header>
-    <div class="w-full">
-      <a href="#">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <span class="ml-3 text-2xl">Your Company</span>
+  <header class="header">
+    <div class="l-container-lg">
+      <a href="#" class="logo">
+        <img src="../assets/images/logo.svg" />
+        <span>Your Company</span>
+      </a>
+
+      <!-- Navigation -->
+      <nav id="navigation" class="nav">
+        <ul class="menu">
+          <li>
+            <a href="#" title="關於本區">關於本區</a>
+          </li>
+          <li>
+            <a href="#" title="最新消息">最新消息</a>
+            <ul>
+              <li><a href="#" title="最新消息">最新消息</a></li>
+              <li><a href="#" title="活動資訊">活動資訊</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- Navigation Switch -->
+      <a href="#navigation" class="nav-switch" title="點擊前往主導覽">
+        <div aria-hidden="true"></div>
+        <div aria-hidden="true"></div>
+        <div aria-hidden="true"></div>
       </a>
     </div>
   </header>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/scss/main.css';
+import './assets/scss/style.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
