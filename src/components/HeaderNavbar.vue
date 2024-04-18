b26a1023a7a2b50f2c78fe0002bb7b069b910d74
dora <bigpowerpig@gmail.com>
2024-04-18 17:45:06 +0800

    nav rwd1

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -1,21 +1,22 @@
 <template>
   <header class="header">
-    <div class="container-lg">
+    <div class="w-container">
       <a href="#" class="logo">
         <img src="../assets/images/logo.svg" />
         <span>Your Company</span>
       </a>
 
       <!-- Navigation Switch -->
-      <!-- <a href="#navigation" class="nav-switch" title="點擊前往主導覽">
-        <div aria-hidden="true"></div>
-        <div aria-hidden="true"></div>
-        <div aria-hidden="true"></div>
-      </a> -->
+      <a href="#navigation" class="nav-switch" title="點擊前往主導覽"
+      :class="{'is-open': isOpen}" @click="isOpen = !isOpen">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
 
       <!-- Navigation -->
       <nav id="navigation" class="nav">
-        <ul class="menu">
+        <ul class="menu reset">
           <li>
             <a href="#" title="關於本區">關於本區</a>
           </li>
@@ -34,20 +35,23 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import MobileDetect from 'mobile-detect';
 
 onMounted(() => {
+  // Mobile Detect
   const UA = window.navigator.userAgent;
   const md = new MobileDetect(UA);
   const { body } = document;
-
   if (md.mobile()) {
     body.classList.add('mb');
   } else {
     body.classList.add('pc');
   }
 });
+
+const isOpen = ref(false);
+
 </script>
 <style lang="scss" scoped>
 
