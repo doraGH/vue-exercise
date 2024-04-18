4376b7c0924881ffcdbb22a6c2f68823b0e66692
dora <bigpowerpig@gmail.com>
2024-04-19 00:52:38 +0800

    nav-search

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="w-container">
-      <a href="#" class="logo">
+      <a href="#" class="nav-brand">
         <img src="../assets/images/logo.svg" />
         <span>Your Company</span>
       </a>
@@ -29,6 +29,25 @@
           </li>
         </ul>
       </nav>
+
+      <!-- nav links -->
+      <ul class="nav-links">
+        <li><!-- Search -->
+          <a href="#popup-search" class="search-switch"
+          :class="{'is-open': isOpen}" @click="isOpen = !isOpen">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search" aria-hidden="true"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+          </a>
+          <div id="popup-search" class="search-popup">
+            <label for="search_keyword" class="sr-only">搜尋關鍵字</label>
+            <form action="SearchResult.html" name="form_search" class="search-group">
+              <input type="search" name="search_keyword" class="search-input"
+              autocomplete="off" placeholder="搜尋關鍵字">
+              <button type="submit" class="search-send" title="開始搜尋">
+                <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search" aria-hidden="true"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg></button>
+            </form>
+          </div>
+        </li>
+      </ul>
 
     </div>
   </header>
