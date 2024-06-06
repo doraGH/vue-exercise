dff2e7306c741a0622ba77a8972c54506c972502
dora <bigpowerpig@gmail.com>
2024-06-06 17:48:07 +0800

    iban swiper

--- src/components/IndexBanner.vue
+++ src/components/IndexBanner.vue
@@ -3,17 +3,28 @@
   <section class="i-banner-wrap">
     <div class="i-banner__list swiper-wrapper">
       <!-- swiper-slide 改a標籤 則可整張圖設連結 -->
-      <div class="swiper-slide">
-        <div class="item">
+      <swiper class="swiper-slide"
+      :slides-per-view="1"
+      :space-between="0"
+      :modules="modules"
+      :loop="true"
+      :autoplay="{
+        delay: 5000,
+        disableOnInteraction: false,
+      }"
+      :navigation="true"
+      :pagination="{ clickable: true }"
+      >
+        <swiper-slide class="item">
+          <div class="foreground">
+            <img src="../assets/images/del/ban4_txt.png" alt="(圖)">
+          </div>
           <picture class="background">
             <source srcset="../assets/images/del/ban1s.jpg" media="(max-width: 743px)">
             <img src="../assets/images/del/ban1.jpg" alt="(圖)" width="1920" height="975">
           </picture>
-          <div class="foreground">
-            <img src="../assets/images/del/ban4_txt.png" alt="(圖)">
-          </div>
-        </div>
-      </div>
+        </swiper-slide>
+      </swiper>
       <!-- @ YouTube -->
       <!-- <div class="swiper-slide">
         <div class="item">
@@ -26,12 +37,29 @@
     <!-- <div class="control-box">
       <div class="swiper-dots"></div>
     </div> -->
-    <div class="swiper-button-prev"></div>
+    <!-- <div class="swiper-button-prev"></div>
     <div class="swiper-button-next"></div>
-    <div class="swiper-pagination"></div>
+    <div class="swiper-pagination"></div> -->
     <div class="scrolldown"></div>
   </section>
 </template>
-<script setup>
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      // 要使用的相依模組
+      modules: [Autoplay, Navigation, Pagination],
+    };
+  },
+};
 </script>
